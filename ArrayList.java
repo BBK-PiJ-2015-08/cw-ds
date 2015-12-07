1c3c36bfe35a9d8b24b8e01391443be53d31906b
Jade <jadedickinson@gmail.com>
2015-12-07 19:00:36 +0000

    Starting to write the overrides

---
+++ ArrayList.java
@@ -0,0 +1,22 @@
+public class ArrayList implements List {
+	//No constructors
+
+	@Override
+	public boolean isEmpty() {
+
+	}
+
+	@Override
+	public int size() {
+
+	}
+
+	// nb the below is not using the class, it is indicating that the return value will be an object of type ReturnObject
+	@Override
+	public ReturnObject get(int index) {
+
+	}
+
+	@Override
+	public 
+}
