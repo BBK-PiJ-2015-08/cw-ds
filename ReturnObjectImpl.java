fc321ecaa75ef3c963b40bec326de39aadf18f20
Jade <jadedickinson@gmail.com>
2015-12-07 18:41:25 +0000

    Built returnobjectimpl

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,30 @@
+public class ReturnObjectImpl implements ReturnObject {
+	private Object obj;
+	private ErrorMessage em;
+
+	public ReturnObjectImpl(Object obj) {
+		this.obj = obj;
+		this.em = ErrorMessage.NO_ERROR;
+	}
+
+	public (ErrorMessage em) {
+		this.em = em;
+		this.obj = null;
+	}
+
+	@Override
+	public boolean hasError() {
+		return em != ErrorMesage.NO_ERROR;
+		
+	}
+
+	@Override
+	public ErrorMessage getError() {
+		return em;
+	}
+
+	@Override
+	public Object getReturnValue() {
+		return obj;
+	}
+}
