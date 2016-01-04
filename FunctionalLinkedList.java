08f6982cce4a1a1079983082fc1116fa0266b7bb
Jade <jadedickinson@gmail.com>
2016-01-04 00:17:54 +0000

    Added guidelines as comments

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -7,12 +7,26 @@
  */
 public class FunctionalLinkedList extends LinkedList implements FunctionalList {
     @Override
+    /**
+     * Returns the element at the beginning of the list.
+     *
+     * If the list is empty, an appropriate error is returned.
+     *
+     * @return a copy of the element at the beginning of the list or
+     *         an error if the list is empty.
+     */
     //not done
     public ReturnObject head() {
-        return null;
-    }
 
+    }
     @Override
+    /**
+     * Returns a list with the elements in this list except the
+     * head. The elements must be in the same order. The original list
+     * must not change or be affected by changes in the new list.
+     *
+     * If the list is empty, another empty list is returned.
+     */
     //not done
     public FunctionalList rest() {
         return null;
