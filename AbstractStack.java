b813e0a148a980dcc7b210483a7711e28d5f06e0
Jade <jadedickinson@gmail.com>
2016-01-04 18:46:40 +0000

    Removed Overrides added on accident

--- AbstractStack.java
+++ AbstractStack.java
@@ -32,30 +32,5 @@ public abstract class AbstractStack implements Stack {
     public AbstractStack(List list) {
 	  this.internalList = list;
     }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void push(Object item) {
-
-    }
-
-    @Override
-    public ReturnObject top() {
-        return null;
-    }
-
-    @Override
-    public ReturnObject pop() {
-        return null;
-    }
 }
 
