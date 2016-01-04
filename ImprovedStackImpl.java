31bf208ce6ec80255a1a1f0507ec90ea4da85a2e
Jade <jadedickinson@gmail.com>
2016-01-04 19:15:58 +0000

    Added comments indicating requirements

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -2,45 +2,98 @@
  * This class must have only one constructor with only one parameter
  * of type List.
  */
+/**
+ * An implementation of a stack with additional methods.
+ *
+ * Classes implementing this interface must use a {@see List} as the
+ * underlying data structure to store the elements on the stack.
+ *
+ * Not all operations on a stack will always be successful. For
+ * example, a programmer may try to pop an element from an empty
+ * stack. Since we hace not covered exceptions yet, we need another
+ * mechanism to report errors. In order to do that, methods of this
+ * list will return a {@see ReturnObject} that will contain either an
+ * object or an error value of the right kind (as defined in {@see
+ * ErrorMessage}).
+ *
+ */
 public class ImprovedStackImpl implements ImprovedStack {
     @Override
-    //not done
+    /**
+     * Returns a copy of this stack with the items reversed, the top
+     * elements on the original stack is at the bottom of the new
+     * stack and viceversa.
+     *
+     * @return a copy of this stack with the items reversed.
+     */
     public ImprovedStack reverse() {
         return null;
     }
 
     @Override
-    //not done
+    /**
+     * Removes the given object from the stack if it is
+     * there. Multiple instances of the object are all removed.
+     *
+     * Classes implementing this method must use method .equals() to
+     * check whether the item is in the stack or not.
+     *
+     * @param object the object to remove
+     */
     public void remove(Object object) {
 
     }
 
     @Override
-    //not done
+    /**
+     * Returns true if the stack is empty, false otherwise.
+     *
+     * @return true if the stack is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return false;
     }
 
     @Override
-    //not done
+    /**
+     * Returns the number of items currently in the stack.
+     *
+     * @return the number of items currently in the stack
+     */
     public int size() {
         return 0;
     }
 
     @Override
-    //not done
+    /**
+     * Adds an element at the top of the stack.
+     *
+     * @param item the new item to be added
+     */
     public void push(Object item) {
 
     }
 
     @Override
-    //not done
+    /**
+     * Returns the element at the top of the stack. The stack is
+     * left unchanged.
+     *
+     * @return If stack is not empty, the item on the top is returned. If the
+     *         stack is empty, an appropriate error.
+     */
     public ReturnObject top() {
         return null;
     }
 
     @Override
-    //not done
+    /**
+     * Returns the element at the top of the stack. The element is
+     * removed frmo the stack.
+     *
+     * @return If stack is not empty, the item on the top is returned. If the
+     *         stack is empty, an appropriate error.
+     */
     public ReturnObject pop() {
         return null;
     }
