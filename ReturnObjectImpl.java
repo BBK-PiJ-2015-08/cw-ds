e1ea7bd70f6a2bd3518de85c1b58e6bbea79d3a1
Jade <jadedickinson@gmail.com>
2016-01-04 17:46:17 +0000

    Realised I hadn't completed this. Had a stab.

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -11,30 +11,56 @@ public class ReturnObjectImpl implements ReturnObject {
 
 	public ReturnObjectImpl(Object obj) {
 		this.obj = obj;
-		//this.em = ErrorMessage.NO_ERROR;
+		this.err = ErrorMessage.NO_ERROR;
 	}
 
 	public ReturnObjectImpl(ErrorMessage err) {
+		this.obj = null;
 		this.err = err;
 	}
 
-	//public (ErrorMessage em) {
-	//	this.obj = em;
-	//	this.obj = null;
-	//}
-
 	@Override
+
+	/**
+	 * Returns whether there has been an error
+	 * @return whether there has been an error
+	 */
 	public boolean hasError() {
-		return false;
+		if (error != ErrorMessage.NO_ERROR); {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
+
+	/**
+	 * Returns the error message.
+	 *
+	 * This method must return NO_ERROR if and only if
+	 * {@hasError} returns false.
+	 *
+	 * @return the error message
+	 */
 	public ErrorMessage getError() {
-		return null;
+		return err;
 	}
 
 	@Override
+	/**
+	 * Returns the object wrapped in this ReturnObject, i.e. the
+	 * result of the operation if it was successful, or null if
+	 * there has been an error.
+	 *
+	 * Note that the output of this method must be null if {@see
+	 * hasError} returns true, but the opposite is not true: if
+	 * {@see hasError} returns false, this method may or may not
+	 * return null.
+	 *
+	 * @return the return value from the method or null if there has been an error
+	 */
 	public Object getReturnValue() {
-		return null;
+		return obj;
 	}
 }
